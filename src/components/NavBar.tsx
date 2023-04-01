6c104444acac3d4770b68ed0901033acda12f36c
jaussems <jannesaussems@gmail.com>
2023-04-01 20:56:26 +0200

    adding two nav bars, one shown for desktop and one shown for smaller devices

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,16 @@ import styles from "../styles/NavBar.module.scss";
 const NavBar: React.FC<any> = (props: PropsWithChildren) => {
   return (
     <div className={styles.navbar}>
-      <nav>
+      <nav className={styles.navbar__desktop}>
+        <ul>
+          <li>Thema's</li>
+          <li>WERKplaats</li>
+          <li>Nieuws</li>
+          <li>Materialen</li>
+          <li>Community</li>
+        </ul>
+      </nav>
+      <nav className={styles.navbar__mobile}>
         <ul>
           <li>Thema's</li>
           <li>WERKplaats</li>
