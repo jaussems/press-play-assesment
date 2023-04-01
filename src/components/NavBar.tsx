55f00974e2a4d541b1f9532f7518fc29fba5a381
jaussems <jannesaussems@gmail.com>
2023-04-01 20:51:34 +0200

    addig navbar for mobile and desktop

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,20 @@
 import { PropsWithChildren } from "react";
-import styles from "../styles/components/NavBar.module.scss";
+import styles from "../styles/NavBar.module.scss";
 
 const NavBar: React.FC<any> = (props: PropsWithChildren) => {
-  return <div className={styles.navbar}>Navbar</div>;
+  return (
+    <div className={styles.navbar}>
+      <nav>
+        <ul>
+          <li>Thema's</li>
+          <li>WERKplaats</li>
+          <li>Nieuws</li>
+          <li>Materialen</li>
+          <li>Community</li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default NavBar;
