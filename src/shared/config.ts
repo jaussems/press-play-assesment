f9a651e341203ddd23d6396cc80844ed2dfdaf65
jaussems <jannesaussems@gmail.com>
2023-04-04 16:14:26 +0200

    added themes to config

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -3,3 +3,17 @@ import { IArrayOfTags } from "./models";
 export const ArticleTags = {
   ArticleOneTags: [{ text: "Innovatie" }, { text: "EUR" }],
 };
+
+export const ThemeObjects = [
+  { icon: "suitcase.svg", text: "LOB" },
+  { icon: "education.svg", text: "Ho-vaardigheden" },
+  { icon: "algebra.svg", text: "Vakinhoudelijke aansluiting wiskunde" },
+  {
+    icon: "chat.svg",
+    text: "Vakinhoudelijke aansluiting Nederlands/communicatie",
+  },
+  { icon: "worldwide.svg", text: "Vakinhoudelijke aansluiting economie" },
+  { icon: "trade.svg", text: "Doorstroom in cijfers" },
+  { icon: "searchglass.svg", text: "PWS" },
+  { icon: "science.svg", text: "Werkplaats" },
+];
